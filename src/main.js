275f028c4f4a1b3a6d95d1aa5a49d760ce989bf9
jack schuldenfrei <jack2ky@yahoo.com>
2017-01-19 10:17:14 -0500

    added sample file

---
+++ src/main.js
@@ -0,0 +1,16 @@
+"use strict";
+
+//ES6 version
+for (let i = 0; i < 10; i++) {
+    setTimeout(function () {
+        console.log(i);
+    }, 500);
+}
+//ES5 version
+for (var i = 0; i < 10; i++) {
+    (function (j) {
+        setTimeout(function () {
+            console.log(j);
+        }, 500);
+    })(i);
+}
